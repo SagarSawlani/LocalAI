b329dcc00f181efec57778e75c0e7385210462b4
SagarSawlani <sawlanisagar1@gmail.com>
2026-07-13 01:40:53 +0530

    retrieval test

--- backend/document_search/test_search.py
+++ backend/document_search/test_search.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from embedder import embed_query
 from vector_store import search
 
-query = "what is trade guru?"  # change to something relevant to your test PDFs
+query = "what is the turnboc code to draw a circle?"  # change to something relevant to your test PDFs
 vec = embed_query(query)
 results = search(vec)
 
